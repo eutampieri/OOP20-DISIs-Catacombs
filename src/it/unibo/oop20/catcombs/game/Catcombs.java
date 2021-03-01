61abcbea65265c0a9dd0c9aa1e49434a682d7ae2
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-01 21:51:32 +0100

    Added Hello World

---
+++ src/it/unibo/oop20/catcombs/game/Catcombs.java
@@ -0,0 +1,10 @@
+package it.unibo.oop20.catcombs.game;
+
+public class Catcombs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
---
+++ src/it/unibo/oop20/catcombs/game/Tests.java
@@ -0,0 +1,11 @@
+package it.unibo.oop20.catcombs.game;
+
+
+import org.junit.*;
+
+public class Tests {
+	@Test
+	public void testGameLaunch() {
+		Assert.fail("Not yet implemented");
+	}
+}
