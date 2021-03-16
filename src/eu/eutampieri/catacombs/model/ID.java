d34de6e8b86fecc92903108c838f87652d35f76c
Giacomo Monti <73893935+jackmounts@users.noreply.github.com>
2021-03-16 20:57:57 +0100

    Cherry pick of corrected packages

---
+++ src/eu/eutampieri/catacombs/model/ID.java
@@ -0,0 +1,9 @@
+package eu.eutampieri.catacombs.model;
+
+public enum ID {
+
+	Player(),
+	Enemy(), 
+	Pickup();
+	
+}
---
+++ src/eu/eutampieri/catacombs/model/Player.java
@@ -0,0 +1,9 @@
+package eu.eutampieri.catacombs.model;
+
+public class Player extends GameObject{
+
+	public Player(int x, int y, ID id) {
+		super(x, y, id);
+	}
+	
+}
