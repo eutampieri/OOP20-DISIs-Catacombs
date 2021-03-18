9fbd2221c8bc4b325f14f67902e2bae79a74fc16
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-18 11:16:24 +0100

    Implemented LivingCharacter for Player

--- src/eu/eutampieri/catacombs/model/LivingCharacter.java
+++ src/eu/eutampieri/catacombs/model/LivingCharacter.java
@@ -2,7 +2,7 @@ package eu.eutampieri.catacombs.model;
 
 public interface LivingCharacter {
 	public int getHealth();
-	public int setHealth();
+	public void setHealth(final int health);
 	public default boolean isAlive() {
 		return this.getHealth() > 0;
 	}
--- src/eu/eutampieri/catacombs/model/Player.java
+++ src/eu/eutampieri/catacombs/model/Player.java
@@ -1,9 +1,20 @@
 package eu.eutampieri.catacombs.model;
 
-public class Player extends GameObject{
+public class Player extends GameObject implements LivingCharacter{
+	private int health = 100;
 
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
+	}
+
+	@Override
+	public int getHealth() {
+		return this.health;
+	}
+
+	@Override
+	public void setHealth(final int health) {
+		this.health = health;
 	}
 	
 }
