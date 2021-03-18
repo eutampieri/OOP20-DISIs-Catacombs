ee2ea33dbb78169367477e4538ae3b969dfe67db
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-18 12:39:45 +0100

    Player cannot overflow health
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

--- src/eu/eutampieri/catacombs/model/LivingCharacter.java
+++ src/eu/eutampieri/catacombs/model/LivingCharacter.java
@@ -2,6 +2,9 @@ package eu.eutampieri.catacombs.model;
 
 public interface LivingCharacter {
 	public int getHealth();
+	/**
+	 * Please make sure that the resulting health is always >= 0 and <= 100
+	 */
 	public void setHealth(final int health);
 	public default boolean isAlive() {
 		return this.getHealth() > 0;
--- src/eu/eutampieri/catacombs/model/Player.java
+++ src/eu/eutampieri/catacombs/model/Player.java
@@ -15,6 +15,11 @@ public class Player extends GameObject implements LivingCharacter{
 	@Override
 	public void setHealth(final int health) {
 		this.health = health;
+		if(this.health > 100) {
+			this.health = 100;
+		} else if(this.health < 0) {
+			this.health = 0;
+		}
 	}
 	
 }
