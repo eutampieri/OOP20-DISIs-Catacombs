f57cc6f718a86ef61037a550246fcfc40a0941c1
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-18 11:27:36 +0100

    Enemy is now a LivingCharacter

--- src/eu/eutampieri/catacombs/model/Enemy.java
+++ src/eu/eutampieri/catacombs/model/Enemy.java
@@ -1,10 +1,21 @@
 package eu.eutampieri.catacombs.model;
 
-public class Enemy extends GameObject{ //general enemy for example purpose
-
+public class Enemy extends GameObject implements LivingCharacter{ //general enemy for example purpose
+	private int health;
+	
 	public Enemy(int x, int y, ID id) {
 		super(x, y, id);
-		
+		health = 100;
+	}
+
+	@Override
+	public int getHealth() {
+		return this.health;
+	}
+	
+	@Override
+	public void setHealth(int health) {
+		this.health = health;
 	}
 
 }
