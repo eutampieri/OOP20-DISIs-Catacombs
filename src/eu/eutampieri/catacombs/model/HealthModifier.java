5085725a08310f7e969dad4d4999a27bd05e3ac8
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-18 11:44:40 +0100

    Added health modifier interface
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

---
+++ src/eu/eutampieri/catacombs/model/HealthModifier.java
@@ -0,0 +1,15 @@
+package eu.eutampieri.catacombs.model;
+
+public interface HealthModifier {
+	/**
+	 * Method to get how much this object modifies character's health
+	 * @return an integer that, summed to the initial health value, gives the resulting health value
+	 * after the usage of this object
+	 */
+	int getHealthDelta();
+	public default void useOn(LivingCharacter character) {
+		int currentHealth = character.getHealth();
+		currentHealth += this.getHealthDelta();
+		character.setHealth(currentHealth);
+	}
+}
