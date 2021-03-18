437c3df69012ed57e13c731dd063a7590cebb95f
Riccardo Sanità <riccardo.sanita@studio.unibo.it>
2021-03-18 11:37:05 +0100

    Added abstract class State tha uses DangeonGame class ( not implemented yet)

---
+++ src/eu/eutampieri/catacombs/ui/State.java
@@ -0,0 +1,20 @@
+package eu.eutampieri.catacombs.ui;
+
+import java.awt.Graphics2D;
+
+public abstract class State {
+	
+	protected DungeonGame game;
+	
+	public State(DungeonGame game) {
+		this.game = game;
+	}
+	
+	public abstract void update(float delta);
+	public abstract void render(Graphics2D g2);
+	
+	public DungeonGame getGame() {
+		return this.game;
+	}
+
+}
