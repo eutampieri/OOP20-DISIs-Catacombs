1177e5cccaa3bd5c9d999b21799769fcc8ee8cb7
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-24 16:01:33 +0100

    Minor changes
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

--- src/main/java/eu/eutampieri/catacombs/model/LivingCharacter.java
+++ src/main/java/eu/eutampieri/catacombs/model/LivingCharacter.java
@@ -1,12 +1,12 @@
 package eu.eutampieri.catacombs.model;
 
 public interface LivingCharacter {
-	public int getHealth();
+	int getHealth();
 	/**
 	 * Please make sure that the resulting health is always >= 0 and <= 100
 	 */
-	public void setHealth(final int health);
-	public default boolean isAlive() {
+	void setHealth(final int health);
+	default boolean isAlive() {
 		return this.getHealth() > 0;
 	}
 }
--- src/test/java/CharactersTests.java
+++ src/test/java/CharactersTests.java
@@ -3,14 +3,23 @@ import eu.eutampieri.catacombs.model.*;
 import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CharactersTests {
 
     @Test
-    void testName() {
+    void testPlayerName() {
         final String name = "John Appleseed";
         Player p = new Player(0, 0, name);
         assertEquals(p.getName(), name);
+    }
+
+    @Test
+    void testPlayerHealth() {
+        final String name = "John Appleseed";
+        Player p = new Player(0, 0, name);
+        assertEquals(p.getHealth(), 100);
+        assertTrue(p.isAlive());
     }
 }
