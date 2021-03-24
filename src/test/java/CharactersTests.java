57c0e804175c555928575baa62260fd30a7cc40c
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-24 15:38:25 +0100

    Added test for player's name getter

--- src/test/java/CharactersTests.java
+++ src/test/java/CharactersTests.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.junit.jupiter.api.Test;
+import eu.eutampieri.catacombs.model.*;
+import org.junit.jupiter.api.TestInstance;
 
-public class CharactersTests {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CharactersTests {
+
+    @Test
+    void testName() {
+        final String name = "John Appleseed";
+        Player p = new Player(0, 0, name);
+        assertEquals(p.getName(), name);
+    }
 }
