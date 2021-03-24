207872e314210112679ee7ec4328da44701d00a9
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-24 11:36:06 +0100

    Moved tests to test package
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

--- src/test/EquipmentTest.java
+++ src/test/EquipmentTest.java
@@ -1,6 +1,7 @@
-package eu.eutampieri.catacombs.model;
+package test;
 
 import org.junit.jupiter.api.Test;
+import eu.eutampieri.catacombs.model.*;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -8,7 +9,7 @@ class EquipmentTest {
 
 	@Test
 	void testSimpleWeapon() {
-		Player p = new Player(0,0, ID.Player);
+		Player p = new Player(0,0, "Alice");
 		final int healthOnCreation = p.getHealth();
 		final HealthModifier h = new Gun(21);
 		h.useOn(p);
@@ -17,7 +18,7 @@ class EquipmentTest {
 	
 	@Test
 	void testHealthUnderflow() {
-		Player p = new Player(0,0, ID.Player);
+		Player p = new Player(0,0, "Bob");
 		final HealthModifier h = new Gun(21);
 		for(int i = 0; i < 5; i++) {
 			h.useOn(p);
@@ -27,7 +28,7 @@ class EquipmentTest {
 	
 	@Test
 	void testHealthOverflow() {
-		Player p = new Player(0,0, ID.Player);
+		Player p = new Player(0,0, "Charles");
 		final HealthModifier h = new SimplePotion(10, "Potion10");
 		h.useOn(p);
 		assertEquals(p.getHealth(), 100);
@@ -35,7 +36,7 @@ class EquipmentTest {
 	
 	@Test
 	void testSimplePotionAfterWeapon() {
-		Player p = new Player(0,0, ID.Player);
+		Player p = new Player(0,0, "Dan");
 		final int healthOnCreation = p.getHealth();
 		final HealthModifier w = new Gun(21);
 		final HealthModifier o = new SimplePotion(20, "Potion20");
---
+++ src/test/MapTests.java
@@ -0,0 +1,13 @@
+package eu.eutampieri.catacombs.model.map;
+
+
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.Assert.fail;
+
+class Tests {
+    @Test
+    void testGameLaunch() {
+        fail("Not yet implemented");
+    }
+}
