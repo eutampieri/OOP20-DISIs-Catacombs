a7dfa421f4cfdca5841422edc59e6c2e72624a3a
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-25 16:12:17 +0100

    Fix Player for checkstyle
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

--- src/main/java/eu/eutampieri/catacombs/model/Player.java
+++ src/main/java/eu/eutampieri/catacombs/model/Player.java
@@ -1,13 +1,13 @@
 package eu.eutampieri.catacombs.model;
 
-public class Player extends GameObject implements LivingCharacter{
+public final class Player extends GameObject implements LivingCharacter {
     private static final int BASE_MOVEMENT_SPEED = 2;
     private static final int MAX_BASE_HP = 100;
     private static final int MIN_BASE_HP = 0;
     private int health;
     private final String name;
 
-    public Player(int x, int y, String name) {
+    public Player(final int x, final int y, final String name) {
         super(x, y, ID.PLAYER);
         setVelX(BASE_MOVEMENT_SPEED);
         setVelY(BASE_MOVEMENT_SPEED);
@@ -23,9 +23,9 @@ public class Player extends GameObject implements LivingCharacter{
     @Override
     public void setHealth(final int health) {
         this.health = health;
-        if(this.health > this.MAX_BASE_HP) {
+        if (this.health > this.MAX_BASE_HP) {
             this.health = this.MAX_BASE_HP;
-        } else if(this.health < this.MIN_BASE_HP) {
+        } else if (this.health < this.MIN_BASE_HP) {
             this.health = this.MIN_BASE_HP;
         }
 
