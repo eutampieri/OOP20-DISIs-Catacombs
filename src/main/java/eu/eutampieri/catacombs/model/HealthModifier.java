f7b2443a4c85336a66cab693d82b81d511d87074
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-25 16:05:24 +0100

    Fix HealthModifier for checkstyle
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

--- src/main/java/eu/eutampieri/catacombs/model/HealthModifier.java
+++ src/main/java/eu/eutampieri/catacombs/model/HealthModifier.java
@@ -2,16 +2,16 @@ package eu.eutampieri.catacombs.model;
 
 public interface HealthModifier {
     /**
-     * Method to get how much this object modifies character's health
-     * @return an integer that, summed to the initial health value, gives the resulting health value
-     * after the usage of this object
+     * Method to get how much this object modifies character's health.
+     * @return an integer that, summed to the initial health value, gives
+     * the resulting health value after the usage of this object
      */
     int getHealthDelta();
-    public default void useOn(LivingCharacter character) {
+    default void useOn(LivingCharacter character) {
         int currentHealth = character.getHealth();
         currentHealth += this.getHealthDelta();
         character.setHealth(currentHealth);
     }
 
-    public String getName();
+    String getName();
 }
