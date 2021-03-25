2288d213b4aabbf7f7b174c51acb27f912ef3b70
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-25 16:09:38 +0100

    Fix LivingCharacter for checkstyle
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

--- src/main/java/eu/eutampieri/catacombs/model/LivingCharacter.java
+++ src/main/java/eu/eutampieri/catacombs/model/LivingCharacter.java
@@ -1,12 +1,13 @@
 package eu.eutampieri.catacombs.model;
 
 public interface LivingCharacter {
-	int getHealth();
-	/**
-	 * Please make sure that the resulting health is always >= 0 and <= 100
-	 */
-	void setHealth(final int health);
-	default boolean isAlive() {
-		return this.getHealth() > 0;
-	}
+    int getHealth();
+    /**
+     * Please make sure that the resulting health is always >= 0 and <= 100.
+     * @param health the new health value
+     */
+    void setHealth(int health);
+    default boolean isAlive() {
+        return this.getHealth() > 0;
+    }
 }
