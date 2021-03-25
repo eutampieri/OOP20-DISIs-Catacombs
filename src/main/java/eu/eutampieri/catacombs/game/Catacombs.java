8fa0727cebbebb1f4c640fa5446c9244c2fdabde
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-25 15:09:18 +0100

    Removed tabs in Catacombs.java
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

--- src/main/java/eu/eutampieri/catacombs/game/Catacombs.java
+++ src/main/java/eu/eutampieri/catacombs/game/Catacombs.java
@@ -2,7 +2,7 @@ package eu.eutampieri.catacombs.game;
 
 public class Catacombs {
     public static void main(String[] args) {
-	    // TODO Auto-generated method stub
+        // TODO Auto-generated method stub
         // Removing hello world because of PMD
         // System.out.println("Hello, World!");
 	}
