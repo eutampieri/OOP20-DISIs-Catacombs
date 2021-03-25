cb058054c8377dde5146f26b189583c8e50aa173
Riccardo Sanità <riccardo.sanita@studio.unibo.it>
2021-03-25 17:35:38 +0100

    optimized AssetManager

--- src/main/java/eu/eutampieri/catacombs/ui/gamefx/AssetManager.java
+++ src/main/java/eu/eutampieri/catacombs/ui/gamefx/AssetManager.java
@@ -33,9 +33,25 @@ public class AssetManager {
 	}
 	
 	public void load() {
-		loadSlimeAnimations();
-		loadBatAnimations();
-		loadPlayerAnimations();
+		loadAnimations("Walk_up", "/playersheet.png", 8, 96, 32, false);
+		loadAnimations("Walk_down", "/playersheet.png", 8, 64, 32, false );
+		loadAnimations("Walk_left", "/playersheet.png", 8, 32, 32, false );
+		loadAnimations("Walk_right", "/playersheet.png",8, 0, 32, false );
+
+		loadAnimations("Die", "/playersheet.png", 8, 128, 32, false );
+
+		loadAnimations("Attack_up", "/playersheet.png", 8, 160, 32, false );
+		loadAnimations("Attack_down", "/playersheet.png", 8, 192, 32, false );
+		loadAnimations("Attack_left", "/playersheet.png", 8, 256, 32, false );
+		loadAnimations("Attack_right", "/playersheet.png", 8, 224, 32, false );
+
+		loadAnimations("Slime_up", "/slimesheet.png", 4, 63, 32, false );
+		loadAnimations("Slime_down", "/slimesheet.png", 4, 0, 32, false );
+		loadAnimations("Slime_left", "/slimesheet.png", 4, 96, 32, false );
+		loadAnimations("Slime_right", "/slimesheet.png", 4, 32, 32, false );
+
+		loadAnimations("Bat_right", "/batsheet.png", 3, 224, 32, false );
+		loadAnimations("Bat_left", "/batsheet.png", 3, 224, 32, true );
 		loadImages();
 	}
 	
@@ -89,108 +105,19 @@ public class AssetManager {
 		allImages.put("potion", tileSheet.cutImage(0,181,9,11));
 		allImages.put("gun", tileSheet.cutImage(0, 193, 21, 17));
 	}
-	
-	public void loadPlayerAnimations() {
-		// Player Animations
-		GameSheets playerSheet = new GameSheets("/playersheet.png");
 
-		// walk animations
-		BufferedImage[] playerUp, playerDown, playerRight ,playerLeft;
-		int numFramesPlayer = 8;
+	public void loadAnimations(String name, String image, int numFrames, int offset, int dimension, boolean flip) {
+		GameSheets sheet = new GameSheets(image);
+		BufferedImage[] res = new BufferedImage[numFrames];
+		for (int i = 0; i < numFrames; i++) {
+			if (!flip){
+				res[i] = sheet.cutImage(dimension * i, offset * dimension, dimension, dimension);
+			} else {
+				res[i] = horizontalFlip(sheet.cutImage(dimension * i, offset * dimension, dimension, dimension));
+			}
 
-		playerUp = new BufferedImage[numFramesPlayer];
-		playerDown = new BufferedImage[numFramesPlayer];
-		playerRight = new BufferedImage[numFramesPlayer];
-		playerLeft = new BufferedImage[numFramesPlayer];
-		
-		for (int i = 0; i < numFramesPlayer; i++) {
-			playerUp[i] = playerSheet.cutImage(32 * i, 96, 32, 32);
-			playerDown[i] = playerSheet.cutImage(32 * i, 64, 32, 32);
-			playerRight[i] = playerSheet.cutImage(32 * i, 0 , 32, 32);
-			playerLeft[i] = playerSheet.cutImage(32 * i, 32, 32, 32);
 		}
-		
-		allAnimations.put("walk_up", playerUp);
-		allAnimations.put("walk_down", playerDown);
-		allAnimations.put("walk_right", playerRight);
-		allAnimations.put("walk_left", playerLeft);
-
-		// die animations
-		BufferedImage[] die;
-
-		die = new BufferedImage[numFramesPlayer];
-		
-		for (int i = 0; i < numFramesPlayer; i++) {
-			die[i] = playerSheet.cutImage(32 * i, 128 , 32, 32);
-		}
-		
-		allAnimations.put("die", die);
-
-		// attack animations
-		BufferedImage[] attackUp, attackDown, attackLeft, attackRight;
-		attackUp = new BufferedImage[numFramesPlayer];
-		attackDown = new BufferedImage[numFramesPlayer];
-		attackRight = new BufferedImage[numFramesPlayer];
-		attackLeft = new BufferedImage[numFramesPlayer];
-		
-		for (int i = 0; i < numFramesPlayer; i++) {
-			attackUp[i] = playerSheet.cutImage(32 * i, 160, 32, 32);
-			attackDown[i] = playerSheet.cutImage(32 * i, 192, 32, 32);
-			attackRight[i] = playerSheet.cutImage(32 * i, 224, 32, 32);
-			attackLeft[i] = playerSheet.cutImage(32 * i, 256, 32, 32);
-		}
-		
-		allAnimations.put("attack_up", attackUp);
-		allAnimations.put("attack_down", attackDown);
-		allAnimations.put("attack_Right", attackRight);
-		allAnimations.put("attack_Left", attackLeft);
-
+		allAnimations.put(name, res);
 	}
 
-	public void loadSlimeAnimations() {
-		// Slime Animations
-
-		GameSheets slimeSheet = new GameSheets("/slimesheet.png");
-		BufferedImage[] slimeUp, slimeDown, slimeRight, slimeLeft;
-		int numFramesSlime = 4;
-
-		slimeUp = new BufferedImage[numFramesSlime];
-		slimeDown = new BufferedImage[numFramesSlime];
-		slimeRight = new BufferedImage[numFramesSlime];
-		slimeLeft = new BufferedImage[numFramesSlime];
-
-		for (int i = 0; i < numFramesSlime; i++) {
-			slimeUp[i] = slimeSheet.cutImage(32 * i, 64, 32, 32);
-			slimeDown[i] = slimeSheet.cutImage(32 * i, 0, 32, 32);
-			slimeRight[i] = slimeSheet.cutImage(32 * i, 32, 32, 32);
-			slimeLeft[i] = slimeSheet.cutImage(32 * i, 96, 32, 32);
-		}
-
-		allAnimations.put("slime_up", slimeUp);
-		allAnimations.put("slime_down", slimeDown);
-		allAnimations.put("slime_right", slimeRight);
-		allAnimations.put("slime_left", slimeLeft);
-	}
-
-	public void loadBatAnimations() {
-		// Bat animation
-
-		GameSheets batSheet = new GameSheets("/batsheet.png");
-		BufferedImage[] batRight ,batLeft;
-		int numFramesBat = 3;
-
-		batRight = new BufferedImage[numFramesBat];
-		batLeft = new BufferedImage[numFramesBat];
-
-		for (int i = 0; i < numFramesBat; i++) {
-			batRight[i] = batSheet.cutImage(16 * i, 0, 16, 16);
-			batLeft[i] = horizontalFlip(batRight[i]);
-		}
-
-		allAnimations.put("bat_right", batRight);
-		allAnimations.put("bat_Left", batLeft);
-
-
-	}
-	
 }
