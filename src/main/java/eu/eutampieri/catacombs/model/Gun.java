80dd277b69fc6eeeffebd8214f7630371b6fc980
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-26 10:44:31 +0100

    Refactored Gun
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

--- src/main/java/eu/eutampieri/catacombs/model/Gun.java
+++ src/main/java/eu/eutampieri/catacombs/model/Gun.java
@@ -1,22 +1,22 @@
 package eu.eutampieri.catacombs.model;
 
-public class Gun extends SimpleWeapon{
+public class Gun extends SimpleWeapon {
 
-	private static final String WEAPON_NAME = "Gun";
+    private static final String WEAPON_NAME = "Gun";
 
-	public Gun(int damage, String name, int fireRate, int range, int magazine) {
-		super(damage, name, fireRate, range, magazine);
-	}
+    public Gun(final int damage, final  String name, final  int fireRate, final  int range, final int magazine) {
+        super(damage, name, fireRate, range, magazine);
+    }
 
-	@Override
-	public void update(int delta) {
+    @Override
+    public void update(final int delta) {
 
-	}
+    }
 
-	@Override
-	public void render() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
+
+    }
 
 }
