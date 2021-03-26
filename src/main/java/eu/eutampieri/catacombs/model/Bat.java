f80f3ac6d5077b4e6eb2698b6d9ca8bcc3915d9e
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-03-26 10:39:19 +0100

    Refactored Bat, CollisionBox, Entity
    Signed-off-by: Eugenio Tampieri <eugenio@eutampieri.eu>

--- src/main/java/eu/eutampieri/catacombs/model/Bat.java
+++ src/main/java/eu/eutampieri/catacombs/model/Bat.java
@@ -3,51 +3,51 @@ package eu.eutampieri.catacombs.model;
 import eu.eutampieri.catacombs.model.map.TileMap;
 
 /**
- * Enemy Bat class
+ * Enemy Bat class.
  *
  */
-public class Bat extends Entity {
+public final class Bat extends Entity {
 
-	private SimpleWeapon weapon;
-	private boolean isMoving;
-	private float moveDelay;
+    private SimpleWeapon weapon;
+    private boolean isMoving;
+    private float moveDelay;
 
-	public Bat(int x, int y, TileMap tileMap) {
-		super(x, y, tileMap);
-		setHeight(16);
-		setWidth(16);
-		setBothVelAsEquals(1);
-		setHealth(8);
+    public Bat(final int x, final int y, final TileMap tileMap) {
+        super(x, y, tileMap);
+        setHeight(16);
+        setWidth(16);
+        setBothVelAsEquals(1);
+        setHealth(8);
 
-		// TODO Animations
+        // TODO Animations
 
-		// box = new CollisionBox();
+        // box = new CollisionBox();
 
-		// TODO Enemy targeting box
-	}
+        // TODO Enemy targeting box
+    }
 
-	@Override
-	public void update(int delta) {
-		// TODO Auto-generated method stub
+    @Override
+    public void update(final int delta) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public void render() {
-		// TODO Auto-generated method stub
+    @Override
+    public void render() {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	@Override
-	public int getHealth() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getHealth() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public void setHealth(int health) {
-		// TODO Auto-generated method stub
+    @Override
+    public void setHealth(final int health) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 }
