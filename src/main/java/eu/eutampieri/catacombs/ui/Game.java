097614d06086cb35eab705150c4f017250b59676
Riccardo Sanità <riccardo.sanita@studio.unibo.it>
2021-04-01 15:29:00 +0200

    fixed pmds
    Signed-off-by: Riccardo Sanità <riccardo.sanita@studio.unibo.it>

--- src/main/java/eu/eutampieri/catacombs/ui/Game.java
+++ src/main/java/eu/eutampieri/catacombs/ui/Game.java
@@ -36,11 +36,11 @@ public abstract class Game implements Runnable {
     private Graphics2D graphics;
     private int fps;
     private Thread gameThread;
-    private boolean isTest;
+    /*private boolean isTest;
 
     public void setTest() {
         this.isTest = true;
-    }
+    }*/
 
     public Graphics2D getGraphics() {
         return this.graphics;
