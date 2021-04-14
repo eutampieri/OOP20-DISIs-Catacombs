8953837e49c712505c481fd3a21d8fd761fc72e5
Giacomo Monti <73893935+jackmounts@users.noreply.github.com>
2021-04-14 18:14:55 +0200

    Added Camera class

---
+++ src/main/java/eu/eutampieri/catacombs/model/Camera.java
@@ -0,0 +1,38 @@
+package eu.eutampieri.catacombs.model;
+
+import eu.eutampieri.catacombs.ui.GameConfiguration;
+
+public final class Camera {
+
+    private int xOffset;
+    private int yOffset;
+    private GameConfiguration game;
+
+    private final int mapWidth, mapHeight;
+
+    public Camera(final int xOffset, final int yOffset, final int mapWidth, final int mapHeight) {
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+        this.mapWidth = mapWidth; //should be multiplied by tile dimensions
+        this.mapHeight = mapHeight;
+    }
+
+    /**
+     * Centers the camera onto a specified Entity.
+     * @param e Entity to center the camera upon
+     */
+    public void centerOnEntity(final Entity e) {
+        xOffset = e.getPosX() - (game.getGameWidth() / 2);
+        yOffset = e.getPosY() - (game.getGameHeight() / 2);
+        if (xOffset < 0) {
+            xOffset = 0;
+        } else if (xOffset > (mapWidth - game.getGameWidth())) {
+            xOffset = mapWidth - game.getGameWidth();
+        }
+        if (yOffset < 0) {
+            yOffset = 0;
+        } else if (yOffset > mapHeight - game.getGameWidth()) {
+            yOffset = mapHeight - game.getGameHeight();
+        }
+    }
+}
