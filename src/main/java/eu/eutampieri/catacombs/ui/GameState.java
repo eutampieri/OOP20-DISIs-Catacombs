7b34b3ca085fb698e114bbb9a544c3a398a52393
MyK_00L <michaelchelli00@gmail.com>
2021-04-16 22:06:29 +0200

    render map

--- src/main/java/eu/eutampieri/catacombs/ui/GameState.java
+++ src/main/java/eu/eutampieri/catacombs/ui/GameState.java
@@ -1,57 +1,61 @@
 package eu.eutampieri.catacombs.ui;
 
-
+import eu.eutampieri.catacombs.model.GameObject;
+import eu.eutampieri.catacombs.model.map.TileMapFactory;
+import eu.eutampieri.catacombs.model.map.TileMapFactoryImpl;
 import eu.eutampieri.catacombs.ui.utils.FontUtils;
-
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 public class GameState extends State {
 
-    /*private World world;
-    private Player player;*/
+    private final World world;
+    // private Player player;
     private boolean paused;
 
     private final Font font = new Font("Arial", Font.BOLD, 15);
     private final Font pauseFont = new Font("Monospace", Font.BOLD, 30);
     private final DungeonGame game;
 
-    public GameState(final DungeonGame game){
+    public GameState(final DungeonGame game) {
         super(game);
         this.game = game;
-        //TODO  WorldLoader class
-        //this.player = world.getPlayer();
+        final TileMapFactory tmf = new TileMapFactoryImpl();
+        this.world = new World(tmf.def(), new ArrayList<GameObject>());
+        // TODO WorldLoader class
+        // this.player = world.getPlayer();
     }
 
     @Override
-    public void update(final long delta){
-        if(Game.KEY_MANAGER.isKeyJustPressed(KeyEvent.VK_ESCAPE)) {
+    public void update(final long delta) {
+        if (Game.KEY_MANAGER.isKeyJustPressed(KeyEvent.VK_ESCAPE)) {
             this.paused = !paused;
         }
-        if (this.paused){
+        if (this.paused) {
             return;
         }
         // WorldLoader needed
-        //this.world.update(delta);
+        this.world.update(delta);
     }
 
     @Override
-    public void render(final Graphics2D g2){
+    public void render(final Graphics2D g2) {
         // WorldLoader needed
-        //this.world.render(g2);
+        this.world.render(g2);
         g2.setColor(Color.orange);
         g2.setFont(font);
-        g2.drawString("HEALTH : "/* + (int) this.player.getHealth()*/, 5, font.getSize());
-        //g2.drawString("COINS : " + player.getCoins(), 5, font.getSize() * 2 + 10);
+        g2.drawString("HEALTH : "/* + (int) this.player.getHealth() */, 5, font.getSize());
+        // g2.drawString("COINS : " + player.getCoins(), 5, font.getSize() * 2 + 10);
 
         if (this.paused) {
             g2.setFont(this.pauseFont);
             final String msg = "Paused";
-            final float x = (game.getGameWidth() - FontUtils.getTextWidth(pauseFont, msg))/2f;
-            final float y = (game.getGameHeight() - this.pauseFont.getSize())/2f;
+            final float x = (game.getGameWidth() - FontUtils.getTextWidth(pauseFont, msg)) / 2f;
+            final float y = (game.getGameHeight() - this.pauseFont.getSize()) / 2f;
             g2.drawString("Paused", x, y);
         }
     }
