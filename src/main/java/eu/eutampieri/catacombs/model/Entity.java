d5803c0cf8104f61640faeb8e6d785f757a34847
MyK_00L <michaelchelli00@gmail.com>
2021-04-21 15:21:28 +0200

    removed problematic bound checks

--- src/main/java/eu/eutampieri/catacombs/model/Entity.java
+++ src/main/java/eu/eutampieri/catacombs/model/Entity.java
@@ -102,19 +102,6 @@ public abstract class Entity extends GameObject implements LivingCharacter, Anim
         int maxMovementDown = speedY;
         int maxMovementLeft = speedX;
 
-        if (hitBox.getPosX() - speedX < 0) {
-            maxMovementLeft = hitBox.getPosX();
-        }
-        if (hitBox.getPosX() + hitBox.getWidth()* AssetManagerProxy.getMapTileSize() + speedX > tileMap.width()*AssetManagerProxy.getMapTileSize() - 1) {
-            maxMovementRight = tileMap.width()*AssetManagerProxy.getMapTileSize() - hitBox.getPosX() - hitBox.getWidth() - 1;
-        }
-        if (hitBox.getPosY() - speedY < 0) {
-            maxMovementUp = hitBox.getPosY();
-        }
-        if (hitBox.getPosY() + hitBox.getHeight()*AssetManagerProxy.getMapTileSize() + speedY > tileMap.height()*AssetManagerProxy.getMapTileSize() - 1) {
-            maxMovementDown = tileMap.height()*AssetManagerProxy.getMapTileSize() - hitBox.getPosY() - hitBox.getHeight() - 1;
-        }
-
         if (up) {
             if (!isUpCollision(maxMovementUp)) {
                 hitBox.move(0, -maxMovementUp);
