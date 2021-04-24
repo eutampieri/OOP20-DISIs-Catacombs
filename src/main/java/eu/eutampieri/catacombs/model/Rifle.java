8d73ea3958149de1ee0a0b8808072b35f0f2f421
Giacomo Monti <73893935+jackmounts@users.noreply.github.com>
2021-04-24 11:36:16 +0200

    Added sprites

--- src/main/java/eu/eutampieri/catacombs/model/Rifle.java
+++ src/main/java/eu/eutampieri/catacombs/model/Rifle.java
@@ -1,11 +1,14 @@
 package eu.eutampieri.catacombs.model;
 
 import eu.eutampieri.catacombs.model.map.TileMap;
+import eu.eutampieri.catacombs.ui.gamefx.AssetManagerProxy;
 
 public class Rifle extends Weapon {
     private static final int STRENGTH = 3;
     private static final int FIRE_RATE = 210;
     private static final int PROJECTILE_SPEED = 14;
+    private static final int BOX_WIDTH = (int) (45 * AssetManagerProxy.getWeaponScalingFactor());
+    private static final int BOX_HEIGHT = 17;
 
     /**
      * @param e        Entity using gun
@@ -15,6 +18,6 @@ public class Rifle extends Weapon {
      * @param team     The team, i.e. if it's not an enemy
      */
     public Rifle(final Entity e, final TileMap tm, final int x, final int y, final Team team) {
-        super(e, tm, x, y, STRENGTH, PROJECTILE_SPEED, FIRE_RATE, team);
+        super(e, tm, x, y, STRENGTH, PROJECTILE_SPEED, FIRE_RATE, BOX_WIDTH, BOX_HEIGHT, team);
     }
 }
--- src/main/java/eu/eutampieri/catacombs/model/Weapon.java
+++ src/main/java/eu/eutampieri/catacombs/model/Weapon.java
@@ -76,8 +76,48 @@ public abstract class Weapon extends GameObject {
         this.fireDelayCount = 0;
     }
 
+    /**
+     *
+     * @param e        Entity using weapon
+     * @param tm       Tile map
+     * @param x        X position
+     * @param y        Y position
+     * @param strength Bullet strength
+     * @param ps       Bullet speed
+     * @param fr       Weapon fire rate
+     * @param w        Weapon hit box width
+     * @param h        Weapon hit box height
+     * @param team     Shooting entity team
+     */
     public Weapon(final Entity e, final TileMap tm, final int x, final int y, final int strength, final int ps,
-                  final int fr, final Team team, final GameObjectType kind, final int size) {
+                  final int fr, final int w, final int h, final Team team) {
+        super(x, y, GameObjectType.PICKUP, new CollisionBox(x, y, w, h), team);
+        this.user = e;
+        setTileMap(tm);
+        setStrength(strength);
+        setProjectileSpeed(ps);
+        setFireRate(fr);
+        setFireDelay(Math.round(MINUTE_TO_MILLIS / fireRate));
+        setCanFire(true);
+        this.bulletSize = BULLET_DEFAULT_SIZE;
+        this.fireDelayCount = 0;
+    }
+
+    /**
+     *
+     * @param e             Entity using weapon
+     * @param tm            Tile map
+     * @param x             X position
+     * @param y             Y position
+     * @param strength      Bullet strength
+     * @param ps            Bullet speed
+     * @param fr            Weapon fire rate
+     * @param team          Shooting entity team
+     * @param bulletKind    Bullet kind
+     * @param bulletSize    Bullet size
+     */
+    public Weapon(final Entity e, final TileMap tm, final int x, final int y, final int strength, final int ps,
+                  final int fr, final Team team, final GameObjectType bulletKind, final int bulletSize) {
         super(x, y, GameObjectType.PICKUP, new CollisionBox(x, y, SIZE, SIZE), team);
         this.user = e;
         setTileMap(tm);
@@ -86,8 +126,8 @@ public abstract class Weapon extends GameObject {
         setFireRate(fr);
         setFireDelay(Math.round(MINUTE_TO_MILLIS / fireRate));
         setCanFire(true);
-        this.bulletKind = kind;
-        this.bulletSize = size;
+        this.bulletKind = bulletKind;
+        this.bulletSize = bulletSize;
         this.fireDelayCount = 0;
     }
 
@@ -136,6 +176,12 @@ public abstract class Weapon extends GameObject {
         return this.canFire;
     }
 
+    /**
+     * Fires a bullet.
+     * @param psx   Projectile speed modifier in the X axis (1, -1, 0 usually)
+     * @param psy   Projectile speed modifier in the Y axis (1, -1, 0 usually)
+     * @return      A GameObject list composed of the bullet fired.
+     */
     public final List<GameObject> fire(final int psx, final int psy) {
        final Projectile p;
        if (this.bulletKind != null && bulletSize > 8) {
